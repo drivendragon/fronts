8208647448f235c33ca049e700ff52d2d6ad1741
drivendragon <drivendragon@hotmail.com>
2021-06-18 19:44:20 -0400

    display

---
+++ display.js
@@ -0,0 +1,14 @@
+class SLL {
+    // constructor, other methods, removed for brevity
+    display() {
+    	// neatly display nodes in my list
+    
+    var newNode  = new SSLNode(value);
+        if (this.head == null)
+            this.head = newNode;
+            return this;
+    }
+    newNode.next = this.head;
+    this.head = newNode;
+    return this;
+}
